af64e618961c3df60fc3406927059af52da989de
Georgy Pogorelov <zhpogorelov@yandex.ru>
2021-11-19 18:48:11 +0300

    create check date

--- server/controllers/checkdate.controller.js
+++ server/controllers/checkdate.controller.js
@@ -1,5 +1,28 @@
-const checkDate = async (req, res) => {
+const { Op } = require('sequelize');
+const { Book, Room } = require('../db/models');
 
+const checkDate = async (req, res) => {
+  const {
+    checkInDate,
+    checkOutDate,
+    category,
+  } = req.body;
+
+  try {
+    const books = await Book.findAll({
+      where: {
+        checkOutDate: {
+          [Op.between]: [checkInDate, checkOutDate],
+        },
+        categoryRoom: category,
+      },
+    });
+    if (books.length) {
+      res.status(200).json(books);
+    }
+  } catch (error) {
+    res.status(404).json({ error: 'error' });
+  }
 };
 
 module.exports = {
