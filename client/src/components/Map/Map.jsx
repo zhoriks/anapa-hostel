5c06ffe30355acb805fa8d4f07c82bc3ae282f14
Kate <letterforkatherine@gmail.com>
2021-11-19 18:35:33 +0300

    add slider and test-map

---
+++ client/src/components/Map/Map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Map.module.css';
+
+export default function Map() {
+  return (
+    <div className={styles.mapContainer}>
+      <img src="https://anapatoday.com/f/2016/03/29/1603290024od.jpg" alt="map" />
+    </div>
+  );
+}
--- client/src/components/StartScreen/StartScreen.jsx
+++ client/src/components/StartScreen/StartScreen.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import WelcomeScreen from '../WelcomeScreen/WelcomeScreen.jsx';
+import PhotoSlider from '../PhotoSlider/PhotoSlider.jsx';
 
 import styles from './StartScreen.module.css';
+import Map from '../Map/Map.jsx';
 
 export default function StartScreen() {
   return (
     <div className={styles.startContainer}>
       <div className={styles.startLeftContainer}><WelcomeScreen /></div>
+
       <div className={styles.startRightContainer}>
-        <div className={styles.blay}>pizdec</div>
+        <PhotoSlider />
+        <Map />
       </div>
     </div>
   );
