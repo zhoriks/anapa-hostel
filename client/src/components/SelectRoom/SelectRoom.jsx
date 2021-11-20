74c40aa57487d8de17f091198e5a79801245e61e
Christina <belyakovachristina@rambler.ruwd>
2021-11-20 17:12:09 +0300

    render selected room card

--- client/src/components/SelectRoom/SelectRoom.jsx
+++ client/src/components/SelectRoom/SelectRoom.jsx
@@ -2,8 +2,33 @@ import React from 'react';
 import { BsFillPersonFill } from 'react-icons/bs';
 import { AiFillCalendar } from 'react-icons/ai';
 import styles from './SelectRoom.module.css';
+import SelectedRoom from '../SelectedRoom/SelectedRoom.jsx';
 
-const SelectRoom = () => (
+const SelectRoom = () => {
+  const rooms = [
+    {
+      id: 1,
+      type: 'Эконом',
+      name: 'Lastochka',
+      numberOfBeds: '8',
+      price: '1500',
+    },
+    {
+      id: 2,
+      type: 'Эконом',
+      name: 'Morshoy briz',
+      numberOfBeds: '8',
+      price: '1500',
+    },
+    {
+      id: 3,
+      type: 'Комфорт',
+      name: 'Voshod',
+      numberOfBeds: '4',
+      price: '3000',
+    },
+  ];
+  return (
     <>
       <header className={styles.header}>
         <div className={styles.dateContainer}>
@@ -17,6 +42,11 @@ const SelectRoom = () => (
         <span className={styles.chooseRoom}>Выберите номер</span>
         <hr />
       </header>
+      <div className={styles.roomContainer}>
+        {rooms.map((el) => <SelectedRoom key={el.id} type={el.type} name={el.name}
+          numberOfBeds={el.numberOfBeds} price={el.price} />)}
+      </div>
     </>
-);
+  );
+};
 export default SelectRoom;
