ed0a5a00e4461e983d7ceec34f94f54bdd840026
Georgy Pogorelov <zhpogorelov@yandex.ru>
2021-11-20 17:38:16 +0300

    create create-booking-router and fix db

--- server/controllers/booking.controller.js
+++ server/controllers/booking.controller.js
@@ -24,4 +24,41 @@ const livingNowBooking = async (req, res) => {
   }
 };
 
-module.exports = { allBooking, livingNowBooking };
+const createNewBooking = async (req, res) => {
+  const {
+    guestFirstName,
+    guestLastName,
+    guestPatronymic,
+    checkInDate,
+    checkOutDate,
+    categoryRoom,
+    guestsNumber,
+    email,
+    telephone,
+    status,
+    RoomId,
+    comment,
+  } = req.body;
+  try {
+    const booking = await Book.create({
+      guestFirstName,
+      guestLastName,
+      guestPatronymic,
+      checkInDate,
+      checkOutDate,
+      categoryRoom,
+      guestsNumber,
+      email,
+      telephone,
+      status,
+      RoomId,
+      comment,
+    });
+
+    res.status(200).json(booking);
+  } catch (error) {
+    res.status(404).json({ error: 'error' });
+  }
+};
+
+module.exports = { allBooking, livingNowBooking, createNewBooking };
