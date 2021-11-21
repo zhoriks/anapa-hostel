ca3d457277c3212c0b2e61baeb1fd348f6f283c0
Kate <letterforkatherine@gmail.com>
2021-11-21 21:14:45 +0300

    improve final form, add correct formal for phone number and dates from state

--- client/src/components/BookingFormGuestData/BookingFormGuestData.jsx
+++ client/src/components/BookingFormGuestData/BookingFormGuestData.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import NumberFormat from 'react-number-format';
 
 import styles from './BookingFormGuestData.module.css';
+import months from '../data/helpData/months';
 
 export default function BookingFormGuestData() {
   // const dispatch = useDispatch();
+  // получаем уже имеющиеся данные о бронировании из стейта
   const dataAboutBooking = useSelector((state) => state.bookingForm.list);
+
+  // функция для приведения даты в стейте в более читаемый вид формата '20 Ноября' вместо 2021-11-20
+  const getBookingDate = (fullDate) => {
+    if (!fullDate) {
+      return 'no info';
+    }
+    const day = fullDate.split('-')[2];
+    const month = fullDate.split('-')[1];
+    let monthText;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in months) {
+      if (key === month) {
+        monthText = months[key];
+      }
+    }
+    const textFormatDate = `${day} ${monthText}`;
+    return textFormatDate;
+  };
 
   return (
     <div className={styles.guestDataContainer}>
@@ -13,8 +34,10 @@ export default function BookingFormGuestData() {
 
       <div className={styles.bookingDataFromState}>
         <div className={styles.datesAndGuestsData}>
-          <div>{dataAboutBooking.arrivalDate} -
-            {dataAboutBooking.departureDate}</div>
+          <div>
+            {getBookingDate(dataAboutBooking.arrivalDate)}&nbsp;&mdash;&nbsp;
+            {getBookingDate(dataAboutBooking.departureDate)}
+          </div>
           <div>Гостей: {dataAboutBooking.guestNumber} </div>
         </div>
         <div className={styles.totalPriceData}>Общая стоимость:</div>
@@ -37,7 +60,7 @@ export default function BookingFormGuestData() {
             </div>
             <div>
               <input type="email" placeholder="Электронная почта (email)" inputmode="email" required />
-              <input type="tel" placeholder="+_(___)___-____" inputmode="tel" required />
+              <NumberFormat format="+7 (###) ###-####" mask="_" type="tel" placeholder='+ 7 (___) ___-____' />
             </div>
           </form>
           <div className={styles.checkboxes}>
