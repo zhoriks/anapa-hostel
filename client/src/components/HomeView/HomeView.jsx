99ab9210e04db6ebecf61d1a7def40abf60fe344
Kate <letterforkatherine@gmail.com>
2021-11-22 23:08:22 +0300

    add sstyle and no-vacant-date functional

--- client/src/components/HomeView/HomeView.jsx
+++ client/src/components/HomeView/HomeView.jsx
@@ -6,9 +6,8 @@ import PhotoBlock from '../PhotoBlock/PhotoBlock.jsx';
 import HostelRooms from '../HostelRooms/HostelRooms.jsx';
 import Benefits from '../Benefits/Benefits.jsx';
 import ReviewTicker from '../ReviewTicker/ReviewTicker.jsx';
-import BookingFormGuestData from '../BookingFormGuestData/BookingFormGuestData.jsx';
-import SelectRoom from '../SelectRoom/SelectRoom.jsx';
-
+// import BookingFormGuestData from '../BookingFormGuestData/BookingFormGuestData.jsx';
+// import SelectRoom from '../SelectRoom/SelectRoom.jsx';
 
 function HomeView() {
   return (
@@ -21,8 +20,8 @@ function HomeView() {
       <Benefits />
       <PhotoBlock />
       <ReviewTicker />
-      <BookingFormGuestData />
-      <SelectRoom />
+      {/* <BookingFormGuestData />
+      <SelectRoom /> */}
     </>
   );
 }
--- client/src/components/SelectRoom/SelectRoom.jsx
+++ client/src/components/SelectRoom/SelectRoom.jsx
@@ -3,6 +3,7 @@ import { BsFillPersonFill } from 'react-icons/bs';
 import { AiFillCalendar } from 'react-icons/ai';
 import { useSelector } from 'react-redux';
 import styles from './SelectRoom.module.css';
+import Navigation from '../Navigation/Navigation.jsx';
 import SelectedRoom from '../SelectedRoom/SelectedRoom.jsx';
 import BookingWelcomeForm from '../BookingWelcomeForm/BookingWelcomeForm.jsx';
 
@@ -10,8 +11,20 @@ const SelectRoom = () => {
   // тут мы заносим в стейт да/нет - будет ли отрисовываться форма с выбором других дат,
   // если все текущие даты заняты
   const [showForm, setShowForm] = useState(false);
+  // const [hasVacantRooms, setHasVacantRooms] = useState(true);
   const date = useSelector((state) => state.bookingForm.list);
   const vacantRooms = useSelector((state) => state.bookingForm.list.vacantRooms);
+
+  // клиент меняет даты по желанию
+  // const changeDatesByWish = () => {
+  //   setShowForm(true);
+  // };
+
+  // клиент меняет даты из-за отсутствия мест
+  // const changeDatesByNeed = () => {
+  //   setShowForm(true);
+  //   setHasVacantRooms(false);
+  // };
 
   // const rooms = [
   //   {
@@ -38,52 +51,60 @@ const SelectRoom = () => {
   // ];
   return (
     <>
-      <header className={styles.header}>
-        <div className={styles.dateContainer}>
-          <div className={styles.date}>
-            <span className={styles.dateText}>
-              <AiFillCalendar className={styles.icon} />
-              с {date.arrivalDate} по {date.departureDate}
-            </span>
-            <span className={styles.dateText}>
-              <BsFillPersonFill className={styles.icon} /> {date.guestNumber} гостя
-            </span>
-          </div>
-        </div>
-        <span className={styles.chooseRoom}>Выберите номер</span>
-        <hr />
-      </header>
-
-      <div className={styles.roomContainer}>
-        {
-          !vacantRooms.length
-            ? <div className={styles.noVacantRoomsBackground}>
-              <div className={styles.noVacantRoomsContainer}>
-                <div className={styles.noVacantRoomsText}>
-                  К сожалению, нет свободных номеров на выбранные вами даты
-                </div>
-                <div className={styles.noVacantRoomsLink} onClick={() => setShowForm(true)}>
-                  Выбрать другие даты
-                </div>
-                {showForm
-                  ? <div className={styles.noVacantRoomsForm}><BookingWelcomeForm /></div>
-                  : <></>}
-              </div>
+      <Navigation />
+      <div className={styles.pageWrapper}>
+        <header className={styles.header}>
+          <div className={styles.title}><h2>Бронирование</h2></div>
+          <div className={styles.dateContainer}>
+            <div className={styles.date}>
+              <span className={styles.dateText}>
+                <AiFillCalendar className={styles.icon} />
+                {date.arrivalDate}&nbsp;&mdash;&nbsp;{date.departureDate}
+              </span>
+              <span className={styles.dateText}>
+                Гостей:&nbsp;<BsFillPersonFill className={styles.icon} />{date.guestNumber}
+              </span>
             </div>
-            : (
-              <>
-                {
-                  vacantRooms.map((room) => <SelectedRoom
-                    key={room.id}
-                    type={room.type}
-                    name={room.name}
-                    numberOfBeds={room.numberOfBeds}
-                    price={room.price}
-                  />)
-                }
-              </>
-            )
-        }
+            <button className={styles.changeDatesBtn}
+              onClick={() => setShowForm(true)}>Изменить даты
+            </button>
+            {/* нужно модальное окно для изменения дат! */}
+          </div>
+          <span className={styles.chooseRoom}>Выберите номер</span>
+          <hr />
+        </header>
+
+        <div className={styles.roomContainer}>
+          {
+            !vacantRooms.length
+              ? <div className={styles.noVacantRoomsBackground}>
+                <div className={styles.noVacantRoomsContainer}>
+                  <div className={styles.noVacantRoomsText}>
+                    К сожалению, нет свободных номеров на выбранные вами даты
+                  </div>
+                  <div className={styles.noVacantRoomsLink} onClick={() => setShowForm(true)}>
+                    Выбрать другие даты
+                  </div>
+                  {showForm
+                    ? <div className={styles.noVacantRoomsForm}><BookingWelcomeForm /></div>
+                    : <></>}
+                </div>
+              </div>
+              : (
+                <>
+                  {
+                    vacantRooms.map((room) => <SelectedRoom
+                      key={room.id}
+                      type={room.type}
+                      name={room.name}
+                      numberOfBeds={room.numberOfBeds}
+                      price={room.price}
+                    />)
+                  }
+                </>
+              )
+          }
+        </div>
       </div>
     </>
   );
