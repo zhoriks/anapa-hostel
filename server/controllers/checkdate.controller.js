6d9ceeab37866ef84ab9e37efe04f0f98523f181
Georgy Pogorelov <zhpogorelov@yandex.ru>
2021-11-22 17:04:41 +0300

    some fix

--- server/controllers/checkdate.controller.js
+++ server/controllers/checkdate.controller.js
@@ -1,36 +1,63 @@
 const { Op } = require('sequelize');
+const Sequelize = require('sequelize');
 const { Book, Room } = require('../db/models');
 
 const checkDate = async (req, res) => {
   const {
     checkInDate,
-    category,
+    checkOutDate,
     guestsNumber,
   } = req.body;
+
+  const bookss = await Book.findAll({
+    where: {
+      checkInDate: {
+        [Op.gte]: checkInDate,
+      },
+      // checkOutDate: {
+      //   [Op.lte]: checkOutDate,
+      // },
+    },
+  });
 
   const books = await Book.findAll({
     where: {
       checkInDate: {
         [Op.gte]: checkInDate,
       },
-      categoryRoom: category,
     },
-    incluxde: Room,
+    attributes: [
+      [Sequelize.fn('SUM', Sequelize.col('Book.guestsNumber')), 'totalInvoiceAmount'],
+    ],
+    include: [
+      {
+        model: Room,
+      },
+    ],
+    group: ['Room.id'],
   });
 
-  const rooms = await Room.findAll({
-    where: {
-      type: category,
-    },
-  });
+  // const rooms = await Room.findAll();
 
-  const allRoomsBad = rooms.reduce((acc, room) => acc + room.numberOfBeds, 0);
-  const allPeopleLive = books.reduce((acc, book) => acc + book.guestsNumber, 0);
-
-  if (allRoomsBad > allPeopleLive + guestsNumber) {
-    return res.status(200).json({ message: 'Мы можем подселить вас на эти даты' });
-  }
-  res.status(200).json({ message: 'Мы не можем подселить вас на эти даты' });
+  // rooms.forEach((room) => {
+  //   let allPeopleLiveInRoom = 0;
+  //   books.forEach((book) => {
+  //     if (room.id === book.RoomId) {
+  //       console.log('Нашлась команата');
+  //       allPeopleLiveInRoom += Number(book.guestsNumber);
+  //     }
+  //   });
+  //   if (allPeopleLiveInRoom + guestsNumber <= room.numberOfBeds) {
+  //     console.log('Добавилась');
+  //     return roomsAviable.push(room);
+  //   }
+  // });
+  // console.log(roomsAviable);
+  return res.status(200).json({ bookss });
+  // if (Number(0) >= Number(allPeopleLive) + Number(guestsNumber)) {
+  //   return res.status(200).json({ message: 'Мы можем подселить вас на эти даты' });
+  // }
+  // res.status(200).json({ message: 'Мы не можем подселить вас на эти даты' });
 };
 
 module.exports = {
