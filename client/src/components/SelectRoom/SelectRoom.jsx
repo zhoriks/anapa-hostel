8292ce906fc23e676dc61d13045fe39f800d2efd
Kate <letterforkatherine@gmail.com>
2021-11-22 17:09:25 +0300

    get data about rooms from state

--- client/src/components/SelectRoom/SelectRoom.jsx
+++ client/src/components/SelectRoom/SelectRoom.jsx
@@ -7,29 +7,31 @@ import SelectedRoom from '../SelectedRoom/SelectedRoom.jsx';
 
 const SelectRoom = () => {
   const date = useSelector((state) => state.bookingForm.list);
-  const rooms = [
-    {
-      id: 1,
-      type: 'Эконом',
-      name: 'Lastochka',
-      numberOfBeds: '8',
-      price: '1500',
-    },
-    {
-      id: 2,
-      type: 'Эконом',
-      name: 'Morshoy briz',
-      numberOfBeds: '8',
-      price: '1500',
-    },
-    {
-      id: 3,
-      type: 'Комфорт',
-      name: 'Voshod',
-      numberOfBeds: '4',
-      price: '3000',
-    },
-  ];
+  const vacantRooms = useSelector((state) => state.bookingForm.list.vacantRooms);
+
+  // const rooms = [
+  //   {
+  //     id: 1,
+  //     type: 'Эконом',
+  //     name: 'Lastochka',
+  //     numberOfBeds: '8',
+  //     price: '1500',
+  //   },
+  //   {
+  //     id: 2,
+  //     type: 'Эконом',
+  //     name: 'Morshoy briz',
+  //     numberOfBeds: '8',
+  //     price: '1500',
+  //   },
+  //   {
+  //     id: 3,
+  //     type: 'Комфорт',
+  //     name: 'Voshod',
+  //     numberOfBeds: '4',
+  //     price: '3000',
+  //   },
+  // ];
   return (
     <>
       <header className={styles.header}>
@@ -37,19 +39,39 @@ const SelectRoom = () => {
           <div className={styles.date}>
             <span className={styles.dateText}>
               <AiFillCalendar className={styles.icon} />
-              с {date.arrivalDate} по {date.departureDate}</span>
+              с {date.arrivalDate} по {date.departureDate}
+            </span>
             <span className={styles.dateText}>
-              <BsFillPersonFill className={styles.icon} /> {date.guestNumber} гостя</span>
+              <BsFillPersonFill className={styles.icon} /> {date.guestNumber} гостя
+            </span>
           </div>
         </div>
         <span className={styles.chooseRoom}>Выберите номер</span>
         <hr />
       </header>
+
       <div className={styles.roomContainer}>
-        {rooms.map((el) => <SelectedRoom key={el.id} type={el.type} name={el.name}
-          numberOfBeds={el.numberOfBeds} price={el.price} />)}
+        {
+          !vacantRooms.length
+            ? <div>К сожалению, нет свободных номеров на выбранные вами даты</div>
+            : (
+              <>
+                {
+                  // vacantRooms.map((el) => <SelectedRoom />)
+                  vacantRooms.map((room) => <SelectedRoom
+                    key={room.id}
+                    type={room.type}
+                    name={room.name}
+                    numberOfBeds={room.numberOfBeds}
+                    price={room.price}
+                  />)
+                }
+              </>
+            )
+        }
       </div>
     </>
   );
 };
+
 export default SelectRoom;
