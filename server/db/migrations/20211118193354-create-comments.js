7c979c328b12e90069454300bfa1e225341561f9
Georgy Pogorelov <zhpogorelov@yandex.ru>
2021-11-22 11:14:31 +0300

    create model,migration and seeder

---
+++ server/db/migrations/20211118193354-create-comments.js
@@ -0,0 +1,38 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      guestName: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      guestTelephone: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      guestPhoto: {
+        type: Sequelize.TEXT,
+      },
+      comment: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  },
+};
