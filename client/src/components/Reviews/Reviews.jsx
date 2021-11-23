1d614e61d332dd555098b62ce9e88fec721d4b66
Christina <belyakovachristina@rambler.ru>
2021-11-23 14:32:09 +0300

    dispay reviews

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import s from './Review.module.css';
 
 const Reviews = () => {
   const reviews = useSelector((state) => state.adminReviews.list);
-  console.log(reviews);
+  // console.log(reviews);
   return (
-    <div>
-      hello
+    <div className={s.mainContainer}>
+      {reviews.map((el) => (
+        <div className={s.container}>
+          <div className={s.clientInfo}>
+            <h3>{el.guestName}</h3>
+            <span>{el.guestTelephone}</span>
+          </div>
+          <div className={s.review}>
+            <p>{el.comment}</p>
+            <div className={s.buttonGroup}>
+              <button className={`${s.button} ${s.buttonAccept}`}>Принять</button>
+              <button className={`${s.button} ${s.buttonDecline}`}>Отклонить</button>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
