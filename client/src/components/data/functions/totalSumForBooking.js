ac3ab6e1d22713016fb24cebb89287f6d732d95c
Kate <letterforkatherine@gmail.com>
2021-11-23 17:46:41 +0300

    improve function for total sum

--- client/src/components/data/functions/totalSumForBooking.js
+++ client/src/components/data/functions/totalSumForBooking.js
@@ -1,5 +1,7 @@
 import moment from 'moment';
 
+// тут мы вычисляем итоговую сумму с учетом кол-ва ночей и цены за номер
+// используем библиотеку moment.js
 const totalSumForBooking = (arrivalDate, departureDate, price) => {
   if (!arrivalDate || !departureDate || !price) {
     return 'no info';
@@ -7,7 +9,8 @@ const totalSumForBooking = (arrivalDate, departureDate, price) => {
   const firstDay = moment(arrivalDate);
   const lastDay = moment(departureDate);
   const nights = lastDay.diff(firstDay, 'days');
-  return nights;
+  const totalSum = (+nights) * (+price);
+  return totalSum;
 };
 
 export default totalSumForBooking;
