9b79468c8331e8a1e1cacc4261797c5e3930d4ce
edw-nmv <eduard_naumov@yahoo.com>
2021-11-23 19:23:05 +0300

    nodemailer added

--- server/controllers/bookingForm.controller.js
+++ server/controllers/bookingForm.controller.js
@@ -1,4 +1,6 @@
+const nodemailer = require('nodemailer');
 const { Book } = require('../db/models');
+const { dateToTextFormat } = require('../data/dateToTextFormat');
 
 const newBook = async (req, res) => {
   // вытаскиваем данные из тела fetch-запроса fetchBookInDB, отправленного с sagaClient
@@ -15,6 +17,10 @@ const newBook = async (req, res) => {
     RoomId,
     comment,
   } = req.body;
+
+  const checkInDateToTEXT = dateToTextFormat(checkInDate);
+  const checkOutDateToTEXT = dateToTextFormat(checkOutDate);
+
   try {
     // создаем в базе данных в таблице Books запись с полученными данными
     await Book.create({
@@ -30,6 +36,31 @@ const newBook = async (req, res) => {
       RoomId,
       comment,
     });
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'anapa.guest.info@gmail.com',
+        pass: 'anapa.guest122021$$',
+      },
+    });
+
+    const mailOptions = {
+      from: 'anapa.guest.info@gmail.com',
+      to: email,
+      subject: 'Anapa Guest House - Информация о бронировании',
+      html: `${guestFirstName}, мы рады, что вы решили провести свой отдых у нас!<br/><br/>
+
+            Вы забронировали ${guestsNumber} места с ${checkInDateToTEXT} по ${checkOutDateToTEXT}.<br/>
+            В ближайшее время с вами свяжется наш менеджер для подтверждения.<br/><br/>
+            Желаем вам отличного отдыха!<br/><br/>
+      
+            С уважением,<br/>
+            команда Anapa Guest House!`,
+    };
+
+    transporter.sendMail(mailOptions);
+
     res.status(200).json({ success: true });
   } catch (error) {
     res.status(404).json({ error: 'error' });
