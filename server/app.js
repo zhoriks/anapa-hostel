986d03eaf41cef290eaa64f8bd7ab2ef8e4d4ff1
Georgy Pogorelov <zhpogorelov@yandex.ru>
2021-11-24 15:53:37 +0300

    fix port again

--- server/app.js
+++ server/app.js
@@ -13,9 +13,8 @@ const app = express();
 
 dotenv.config();
 
-const PORT = process.env.PORT ?? 5001;
+const PORT = process.env.PORT || 5001;
 const {
-
   SESSION_SECRET = 'my_secret',
 } = process.env;
 
