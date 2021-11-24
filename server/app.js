d19bd895a3c71d0aa34810f55fe05d2bd29a1347
Georgy Pogorelov <zhpogorelov@yandex.ru>
2021-11-24 16:16:16 +0300

    fix

--- server/app.js
+++ server/app.js
@@ -41,13 +41,13 @@ app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.use(express.static(path.resolve('./client/build')));
+console.log(path.resolve('../client/build'));
+app.use(express.static(path.resolve('../client/build')));
 
 app.use('/', indexRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve('./client/build/index.html'));
+  res.sendFile(path.resolve('../client/build/index.html'));
 });
 
 app.listen(PORT, () => {
