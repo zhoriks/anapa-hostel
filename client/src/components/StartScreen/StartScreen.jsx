0323fedcc7be76777b462210110752d8996d8b5a
Kate <letterforkatherine@gmail.com>
2021-11-24 10:50:08 +0300

    change logic of photo slider, add photos

--- client/src/components/StartScreen/StartScreen.jsx
+++ client/src/components/StartScreen/StartScreen.jsx
@@ -4,6 +4,7 @@ import PhotoSlider from '../PhotoSlider/PhotoSlider.jsx';
 
 import styles from './StartScreen.module.css';
 import MapYandex from '../MapYandex/MapYandex.jsx';
+import mainSliderImages from '../data/helpData/mainSlider';
 
 export default function StartScreen() {
   return (
@@ -11,7 +12,7 @@ export default function StartScreen() {
       <div className={styles.startLeftContainer}><WelcomeScreen /></div>
 
       <div className={styles.startRightContainer}>
-        <PhotoSlider />
+        <PhotoSlider images={mainSliderImages} />
         <MapYandex />
       </div>
     </div>
---
+++ client/src/components/data/helpData/mainSlider.js
@@ -0,0 +1,17 @@
+import mainSlider1 from '../img/mainSlider/mainSlider1.jpeg';
+import mainSlider2 from '../img/mainSlider/mainSlider2.jpeg';
+import mainSlider3 from '../img/mainSlider/mainSlider3.webp';
+import mainSlider4 from '../img/mainSlider/mainSlider4.jpeg';
+import mainSlider5 from '../img/mainSlider/mainSlider5.jpeg';
+import mainSlider6 from '../img/mainSlider/mainSlider6.jpeg';
+
+const mainSliderImages = [
+  { src: mainSlider1 },
+  { src: mainSlider2 },
+  { src: mainSlider3 },
+  { src: mainSlider4 },
+  { src: mainSlider5 },
+  { src: mainSlider6 },
+];
+
+export default mainSliderImages;
