253d832fb5f3979980d9cde03a33528b7add2cc3
Christina <belyakovachristina@rambler.ru>
2021-11-24 12:34:09 +0300

    availible/taken rooms status displayed in single room component

--- client/src/components/SingleRoom/SingleRoom.js
+++ client/src/components/SingleRoom/SingleRoom.js
@@ -4,25 +4,28 @@ import s from './SingleRoom.module.css';
 const SingleRoom = ({
   name, fullness, number, type,
 }) => (
-    <div className={s.container}>
-      <div className={s.img_container}>
-        <img src="/room.jpeg" alt="" className={s.img} />
-        <span className={s.status}>Доступна</span>
-        <span className={s.room_name}>{name}</span>
-      </div>
-      <div className={s.row_container}>
-        <span>Заполненность</span>
-        <span>{fullness}</span>
-      </div>
-      <div className={s.row_container}>
-        <span>Количество кроватей</span>
-        <span>{number}</span>
-      </div>
-      <div className={s.row_container}>
-        <span>Тип</span>
-        <span>{type}</span>
-      </div>
+  <div className={s.container}>
+    <div className={s.img_container}>
+      <img src="/room.jpeg" alt="" className={s.img} />
+      {fullness === number
+        ? <span className={`${s.status} ${s.taken}`}>Занята</span>
+        : <span className={`${s.status} ${s.available}`}>Доступна</span>
+      }
+      <span className={s.room_name}>{name}</span>
     </div>
+    <div className={s.row_container}>
+      <span>Заполненность</span>
+      <span>{fullness}</span>
+    </div>
+    <div className={s.row_container}>
+      <span>Количество кроватей</span>
+      <span>{number}</span>
+    </div>
+    <div className={s.row_container}>
+      <span>Тип</span>
+      <span>{type}</span>
+    </div>
+  </div>
 );
 
 export default SingleRoom;
