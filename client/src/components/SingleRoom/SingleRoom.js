abfd14826962300192836fe925f9fa27f6c636b7
edw-nmv <eduard_naumov@yahoo.com>
2021-11-25 15:51:26 +0300

    + some changes

--- client/src/components/SingleRoom/SingleRoom.js
+++ client/src/components/SingleRoom/SingleRoom.js
@@ -5,13 +5,14 @@ const SingleRoom = ({
   name, fullness, number, type,
 }) => (
   <div className={s.container}>
+    <span className={s.room_name}>{name}</span>
     <div className={s.img_container}>
       <img src="/room.jpeg" alt="" className={s.img} />
       {fullness === number
         ? <span className={`${s.status} ${s.taken}`}>Занят</span>
         : <span className={`${s.status} ${s.available}`}>Доступен</span>
       }
-      <span className={s.room_name}>{name}</span>
+      {/* <span className={s.room_name}>{name}</span> */}
     </div>
     <div className={s.row_container}>
       <span style={{ fontWeight: 'bold' }}>Заполненность</span>
